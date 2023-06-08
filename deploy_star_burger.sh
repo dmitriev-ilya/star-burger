ffe1a880cdcb1f85412aa80d00c6d5940d040bdc
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-06-08 20:27:50 +0500

    path fixes

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-/usr/bin/git -C ./star-burger pull
-./star-burger/venv/bin/pip install -r ./star-burger/requirements.txt
-./star-burger/venv/bin/python3 ./star-burger/manage.py migrate
-./star-burger/venv/bin/python3 ./star-burger/manage.py collectstatic --noinput
-npm --prefix ./star-burger/ ci --dev
-./star-burger/node_modules/.bin/parcel build ./star-burger/bundles-src/index.js --dist-dir bundles --public-url="./"
+/usr/bin/git -C ./ pull
+./venv/bin/pip install -r ./requirements.txt
+./venv/bin/python3 ./manage.py migrate
+./venv/bin/python3 ./manage.py collectstatic --noinput
+npm --prefix ./ ci --dev
+./node_modules/.bin/parcel build ./bundles-src/index.js --dist-dir bundles --public-url="./"
 systemctl daemon-reload
 systemctl restart starburger
 systemctl reload nginx
 
-source ./star-burger/.env
-git_commit=$(/usr/bin/git -C ./star-burger rev-parse HEAD)
+source ./.env
+git_commit=$(/usr/bin/git -C ./ rev-parse HEAD)
 
 curl -H "X-Rollbar-Access-Token:$ROLLBAR_TOKEN" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy'\
  -d '{"environment":"'$ROLLBAR_ENVIRONMENT'", "revision":"'$git_commit'", "rollbar_name":"'$ROLLBAR_NAME'", "local_username":"'$USER'", "comment": "", "status": "succeeded"}'
